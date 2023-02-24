a4691c8502876237a05a9610fb0d54b7dcd0bc5d
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-02-24 12:30:09 -0800

    adds loop

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
+  
+  const names = ["Charlie", "Samip", "Ali"];
+  const occasion = "birthday";
+  const messages = writeCards(names, occasion);
+  console.log(messages);
+
+
+
+
+
+  function countDown(){
+    let i = 10;
+      while (i >= 0){
+          console.log(i);
+          i--;
+      }
+  }
